b3a8026ae0442e7580a3a776264398e4ab25072e
omdwivedi-web <omdcode@gmail.com>
2026-02-05 09:25:42 +0000

    variables config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accountEmail = "omgoogle.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2// not allowed
+
+
+accountEmail = "om@outlook.com"
+accountPassword = "abcd1234"
+accountCity ="UTTAR PRADESH"
+
+// console.log(accountId)
+console.table([accountId, accountEmail, accountCity, accountPassword, accountState])
+
+
