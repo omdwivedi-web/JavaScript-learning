5e57b61f6e31a1e679eecd99fa65e455fdf8fcba
omdwivedi-web <omdcode@gmail.com>
2026-02-07 13:40:04 +0000

    comparision confi done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 >= 1)
+// console.log(2 <= 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+//console.log("2" > 1) // this will convert the string "2" to number 2 and compare it with number 1 to give output true
+
+//console.log("02" < 1) // always remember that compare same type of data.
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); //( in this case null is converted to 0 and then compared with 0 to give output true)
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0); 
+
+
+//== ka check karne ka way alag hota hai,
+
+//=== 
+
+console.log("2" === 2) //  false because === checks both value and type, and "2" is a string while 2 is a number.
+
+// SUMMERY
+
+// AVOID COMPARING DIFFERENT DATA TYPES, ALWAYS COMPARE SAME DATA TYPES. 
